45d0f95c579c3ac0cd3b2d651293c11ff3d43368
Justin Barber <barber.justin@gmail.com>
2020-10-06 21:03:50 -0400

    add swagger deps

--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule TwoStep.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_swagger, "~> 0.8"},
+      {:ex_json_schema, "~> 0.5"}
     ]
   end
 
